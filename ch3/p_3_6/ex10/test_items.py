e7bd31086dcc19ce880b72b5703149d2c4dba1d5
GitQuasar <cursedonela@yandex.ru>
2025-10-27 04:49:42 +0500

    fix fixture

--- ch3/p_3_6/ex10/test_items.py
+++ ch3/p_3_6/ex10/test_items.py
@@ -1,12 +1,11 @@
 import time
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_item_add_to_basket(browser: webdriver.Chrome):
+def test_item_add_to_basket(browser):
     browser.get(link)
     time.sleep(5)
     basket = browser.find_element(By.CSS_SELECTOR, "form button.btn-add-to-basket")
